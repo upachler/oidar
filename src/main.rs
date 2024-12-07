5377d2131a392a458d2059eb5c8f15de4987c930
Pachler, Uwe <uwe.pachler@datagroup.de>
2024-12-07 14:32:59 +0100

    move adapters to new inbound and outbound modules

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use domain::backend::ports::*;
 use domain::frontend::ports::*;
 
 mod domain;
-mod dummy;
+mod inbound;
+mod outbound;
 
-mod easy_repl;
+
 
 fn main() {
 
@@ -15,15 +16,15 @@ fn main() {
 
     let fm4_url = Url::parse("https://orf-live.ors-shoutcast.at/fm4-q2a").unwrap();
 
-    let loader = dummy::DummyLoader::new();
-    let decoder = dummy::DummyDecoder::new();
-    let player = dummy::DummyPlayer::new();
+    let loader = outbound::dummy::DummyLoader::new();
+    let decoder = outbound::dummy::DummyDecoder::new();
+    let player = outbound::dummy::DummyPlayer::new();
 
     let backend = domain::backend::service::new(loader, decoder, player);
 
-    backend.send_command(BackendCommand::PlayUrl(fm4_url));
+    backend.send_command(BackendCommand::PlayUrl(fm4_url)).unwrap();
     
-    let frontend = easy_repl::EasyReplFrontend::new();
+    let frontend = inbound::easy_repl::EasyReplFrontend::new();
 
     frontend.run(backend);
 }
